27e57bbb566e5f5cb53dc86a4ce969abbfa78d1b
DanielPomfyuk <danielpomfyuk99@gmail.com>
2022-03-02 21:24:14 +0000

    adding login + report logic for one person

--- report_pidars.py
+++ report_pidars.py
@@ -1,14 +1,29 @@
 from selenium import webdriver
 from webpage_elements import *
-import time
+import json
 
+print("Slava Ukrayini")
+with open("./dolboebi.json", "r") as read_file:
+    dolboebi = json.load(read_file)
 
+# set up the web driver
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
-
 driver.get("https://www.instagram.com")
-accept_cookies(driver)
 
+# accept_cookies & log in
+accept_cookies(driver)
+log_in(driver)
+
+# report pidars
+posts_counter = 0
+for index in dolboebi:
+    dolboeb = dolboebi[index]
+    print(f"Reporting this dolboeb: {dolboeb['username']}")
+    for post in dolboeb["posts"]:
+        report_post(driver,post)
+        posts_counter+=1
+    print(f"Posts reported in total: {posts_counter}")
 time.sleep(5)
 driver.quit()
--- webpage_elements.py
+++ webpage_elements.py
@@ -1,10 +1,77 @@
+from os import system
+import time
 from css_selectors import *
+import logging
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+import toml
+
+config = toml.load("./config.toml")
 
 
-def accept_cookies(driver):
-    search = driver.find_element_by_css_selector(ALLOW_COOKIES_BUTTON)
-    search.send_keys(Keys.RETURN)
+def findElement(driver, timeout, css_selector):
+    return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+
+
+def accept_cookies(driver: webdriver):
+    logging.info("accepting cookies. yum yum")
+    cookie_button = findElement(driver, 10, ALLOW_COOKIES_BUTTON)
+    cookie_button.send_keys(Keys.RETURN)
+
+
+def insert_credantials(driver: webdriver):
+    logging.info("logging in using the credentials from ./config.toml")
+    username_input = findElement(driver, 10, USERNAME_INPUT)
+    username_input.send_keys(config.get('credentials').get('username'))
+    password_input = findElement(driver, 10, PASSWORD_INPUT)
+    password_input.send_keys(config.get('credentials').get('password'))
+    submit_button = findElement(driver, 10, SUBMIT_CREDENTIALS_BUTTON)
+    submit_button.click()
+
+
+def wait_for_code(driver: webdriver):
+    logging.info(
+        "Please enter the code ,sent to your phone number.\nWaiting for 2 minutes")
+    dont_save_button = findElement(driver, 120, DONT_SAVE_PASSWORD_BUTTON)
+    dont_save_button.click()
+
+
+def notifications_off(driver: webdriver):
+    notifications_off_button = findElement(
+        driver, 10, DONT_TURN_ON_NOTIFICATIONS_BUTTON)
+    notifications_off_button.click()
+
+
+def log_in(driver: webdriver):
+    try:
+        insert_credantials(driver)
+        wait_for_code(driver)
+        notifications_off(driver)
+    except:
+        print("Something went wrong. Sutting down the browser")
+        driver.quit()
+        system.exit()
+    time.sleep(2)
+
+def report_post(driver: webdriver, link):
+    try:
+        driver.get(link)
+        post = findElement(driver,10,POST)
+        post.click()
+        more_options = findElement(driver,10,MORE_OPTIONS_BUTTON)
+        more_options.click()
+        report_button = findElement(driver,10,REPORT_BUTTON)
+        report_button.click()
+        false_information = findElement(driver,10,FALSE_INFORMATION)
+        false_information.click()
+        politics_reason = findElement(driver,10,POLITICS_REASON)
+        politics_reason.click()
+        close_button = findElement(driver,10,CLOSE_BUTTON)
+        close_button.click()
+    except:
+        print("Something went wrong. Sutting down the browser")
+        driver.quit()
+        system.exit()
