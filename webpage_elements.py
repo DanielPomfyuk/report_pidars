d1e368c32d749667a64879e34b34448f5023944f
DanielPomfyuk <danielpomfyuk99@gmail.com>
2022-03-03 22:05:38 +0000

    making 10 a default timeout + better error handling

--- webpage_elements.py
+++ webpage_elements.py
@@ -9,19 +9,19 @@ from selenium.webdriver.support import expected_conditions as EC
 config = toml.load("./config.toml")
 
 
-def findElementBySelector(driver, timeout, css_selector):
+def findElementBySelector(driver, css_selector, timeout=10):
     return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
 
 
-def findElementByXPath(driver, timeout, xpath):
+def findElementByXPath(driver, xpath, timeout=10):
     return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath)))
 
 
 def accept_cookies(driver: webdriver):
-    logging.info("accepting cookies. yum yum")
+    print("accepting cookies. yum yum")
     try:
         cookie_button = findElementBySelector(
-            driver, 1000, ALLOW_COOKIES_BUTTON)
+            driver, ALLOW_COOKIES_BUTTON)
         cookie_button.click()
     except:
         print("Could not accept the cookies")
@@ -30,23 +30,23 @@ def accept_cookies(driver: webdriver):
 
 
 def insert_credantials(driver: webdriver):
-    logging.info("logging in using the credentials from ./config.toml")
-    username_input = findElementBySelector(driver, 10, USERNAME_INPUT)
+    print("logging in using the credentials from ./config.toml")
+    username_input = findElementBySelector(driver, USERNAME_INPUT)
     username_input.send_keys(config.get('credentials').get('username'))
-    password_input = findElementBySelector(driver, 10, PASSWORD_INPUT)
+    password_input = findElementBySelector(driver, PASSWORD_INPUT)
     password_input.send_keys(config.get('credentials').get('password'))
     submit_button = findElementBySelector(
-        driver, 10, SUBMIT_CREDENTIALS_BUTTON)
+        driver,SUBMIT_CREDENTIALS_BUTTON)
     submit_button.click()
 
 
 def wait_for_code(driver: webdriver):
     try:
-        lock = findElementByXPath(driver, 10, LOCK)
-        logging.info(
+        lock = findElementByXPath(driver, LOCK)
+        print(
             "Please enter the code ,sent to your phone number.\nWaiting for 2 minutes")
         dont_save_button = findElementBySelector(
-            driver, 120, DONT_SAVE_PASSWORD_BUTTON)
+            driver, DONT_SAVE_PASSWORD_BUTTON, 120)
         dont_save_button.click()
     except:
         return
@@ -54,7 +54,7 @@ def wait_for_code(driver: webdriver):
 
 def notifications_off(driver: webdriver):
     notifications_off_button = findElementByXPath(
-        driver, 1000, DONT_TURN_ON_NOTIFICATIONS_BUTTON)
+        driver, DONT_TURN_ON_NOTIFICATIONS_BUTTON)
     notifications_off_button.click()
 
 
@@ -64,20 +64,23 @@ def log_in(driver: webdriver):
         wait_for_code(driver)
         notifications_off(driver)
     except:
-        print("Couldn't Log in. Sutting down the browser")
+        logging.ERROR("Couldn't Log in. Sutting down the browser")
         driver.quit()
         sys.exit()
 
 
 def report_post(driver: webdriver, link):
-    driver.get(link)
-    more_options = findElementBySelector(driver, 10, MORE_OPTIONS_BUTTON)
+    try:
+        driver.get(link)
+    except:
+        return
+    more_options = findElementBySelector(driver, MORE_OPTIONS_BUTTON)
     more_options.click()
-    report_button = findElementByXPath(driver, 10, REPORT_BUTTON)
+    report_button = findElementByXPath(driver, REPORT_BUTTON)
     report_button.click()
-    false_information = findElementByXPath(driver, 10, FALSE_INFORMATION)
+    false_information = findElementByXPath(driver, FALSE_INFORMATION)
     false_information.click()
-    politics_reason = findElementByXPath(driver, 10, POLITICS_REASON)
+    politics_reason = findElementByXPath(driver, POLITICS_REASON)
     politics_reason.click()
-    close_button = findElementBySelector(driver, 10, CLOSE_BUTTON)
+    close_button = findElementBySelector(driver, CLOSE_BUTTON)
     close_button.click()
